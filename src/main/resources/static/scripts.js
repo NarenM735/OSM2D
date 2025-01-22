0b6bbf394472981cc5262a5c9896b4261ce34374
Hrushikesh Patwardhan <hrushikeshp13@gmail.com>
2025-01-22 11:17:08 +0530

    clean up scripts.js

--- src/main/resources/static/scripts.js
+++ src/main/resources/static/scripts.js
@@ -1,76 +1,3 @@
-// var stompClient = null;
-// var notificationCount = 0;
-
-// $(document).ready(function() {
-//     console.log("Index page is ready");
-//     connect();
-
-//     $("#send").click(function() {
-//         sendMessage();
-//     });
-
-//     $("#send-private").click(function() {
-//         sendPrivateMessage();
-//     });
-
-//     $("#notifications").click(function() {
-//         resetNotificationCount();
-//     });
-// });
-
-// function connect() {
-//     var socket = new SockJS('/our-websocket');
-//     stompClient = Stomp.over(socket);
-//     stompClient.connect({}, function (frame) {
-//         console.log('Connected: ' + frame);
-//         updateNotificationDisplay();
-//         stompClient.subscribe('/topic/messages', function (message) {
-//             showMessage(JSON.parse(message.body).content);
-//         });
-
-//         stompClient.subscribe('/user/topic/private-messages', function (message) {
-//             showMessage(JSON.parse(message.body).content);
-//         });
-
-//         stompClient.subscribe('/topic/global-notifications', function (message) {
-//             notificationCount = notificationCount + 1;
-//             updateNotificationDisplay();
-//         });
-
-//         stompClient.subscribe('/user/topic/private-notifications', function (message) {
-//             notificationCount = notificationCount + 1;
-//             updateNotificationDisplay();
-//         });
-//     });
-// }
-
-// function showMessage(message) {
-//     $("#messages").append("<tr><td>" + message + "</td></tr>");
-// }
-
-// function sendMessage() {
-//     console.log("sending message");
-//     stompClient.send("/ws/message", {}, JSON.stringify({'messageContent': $("#message").val()}));
-// }
-
-// function sendPrivateMessage() {
-//     console.log("sending private message");
-//     stompClient.send("/ws/private-message", {}, JSON.stringify({'messageContent': $("#private-message").val()}));
-// }
-
-// function updateNotificationDisplay() {
-//     if (notificationCount == 0) {
-//         $('#notifications').hide();
-//     } else {
-//         $('#notifications').show();
-//         $('#notifications').text(notificationCount);
-//     }
-// }
-
-// function resetNotificationCount() {
-//     notificationCount = 0;
-//     updateNotificationDisplay();
-// }
 
 var stompClient = null;
 
@@ -89,13 +16,6 @@ function connect() {
         clientID = frame.headers["user-name"];
         console.log('Connected: ' + frame);
         onConnected();
-        // stompClient.subscribe('/topic/playerJoin', function (message) {
-        //     showMessage(JSON.parse(message.body).content);
-        // });
-
-        // stompClient.subscribe('/user/topic/private-messages', function (message) {
-        //     showMessage(JSON.parse(message.body).content);
-        // });
         stompClient.subscribe('/topic/gameState', function (message) {
             Playerlist=JSON.parse(message.body);
         });
@@ -114,9 +34,9 @@ function onConnected() {
     });
     // Tell your username to the server
     stompClient.send('/ws/playerJoin',
-        {},
-        JSON.stringify({messageContent: "hello"})
-    )
+		     {},
+		     JSON.stringify({messageContent: "hello"})
+		    )
 
 
     // connectingElement.classList.add('hidden');
@@ -198,26 +118,22 @@ class Bullet {
     }
 }
 
-function renderMessage(){}
-// {
-//     if (frameCount%60==0 && message_time > 0)
-//     {
-// 	fill(100,100,200);
-// 	textSize(20);
-// 	text(show_msg, 5, 25);
-// 	message_time --;
-//     }
-//     if(message_time==0){
-//     show_msg="";}
-// }
-
-
-function draw()
+function renderMessage()
 {
-    background(220);
+    if (frameCount%60==0 && message_time > 0)
+    {
+	fill(100,100,200);
+	textSize(20);
+	text(show_msg, 5, 25);
+	message_time --;
+    }
+    if(message_time==0){
+	show_msg="";
+    }
+}
 
-    let i=0;
-
+function handleMovement()
+{
     if (charIsDown("a"))
 	x_pos -= speed;
     if (charIsDown("d"))
@@ -226,43 +142,31 @@ function draw()
 	y_pos -= speed;
     if (charIsDown("s"))
 	y_pos += speed;
+}
+
+function drawPlayer(player)
+{
+    fill(player.r, player.g, player.b);
+    circle(player.x, player.y, 20);
+}
+
+function draw()
+{
+    background(220);
+
+    
+    handleMovement();
+    
     
     fill(100,100,255);
     
     if(flag==1){
-    stompClient.send('/ws/gameState',
-        {},
-        JSON.stringify({x: x_pos,y:y_pos,name:clientID, r:red, g:green, b:blue})
-    )
-    console.log(JSON.stringify({x: x_pos,y:y_pos,name:clientID,r:red,g:green,b:blue}));
-    
-}
-    
-    if(Playerlist==null){
-        circle(x_pos, y_pos, 20); 
-    }else{
-    // circle(x_pos, y_pos, 20);
-    while(i<Playerlist.length)
-    {
-        var player = null;
-        if(Playerlist[i]!=null){
-            player = Playerlist[i];
-            fill(player.r, player.g, player.b);
-            circle(player.x, player.y, 20);
-        }
-        i++;
+	stompClient.send('/ws/gameState',
+			 {},
+			 JSON.stringify({x: x_pos,y:y_pos,name:clientID, r:red, g:green, b:blue})
+			)
     }
-}
 
-    // if(Bulletlist!=null){
-    // for (let b of Bulletlist)
-    // {
-	// // b.update();
-    // // stompClient.send('/ws/gameState',{},JSON.stringify({x: b.getX(),y:b.getY(),velx: b.getVX(), vely:b.getVY()}));
-    // // stompClient.send('/ws/gameState',{},JSON.stringify({x: b.getX(),y:b.getY()}));
-    // // if()
-	// Bulletlist[b].render();
-    // }
 
     let j=0;
     var bullet=null
@@ -273,37 +177,52 @@ function draw()
             j++;
         }
     }
-
-
-    fill(100,100,200);
     
-    textSize(20);
-    text(show_msg, 5, 25);
-    if (frameCount%60==0 && message_time > 0)
-        {
-        
-        
-        message_time --;
-        }
-        if(message_time==0){
-        show_msg="";}
+    for(const player of Playerlist)
+    {
+	if(!player)
+	    continue;
+
+	if (player.name == clientID) // skip yourself
+	    continue;
+	
+	drawPlayer(player);
+    }
+
+    let self = {};
+    
+    for (const b of bullets)
+    {
+	b.update();
+	b.render();
+    }
+
+    renderMessage();
+
 }
 
 const bullet_speed = 10;
+
+function spawnBullet()
+{
+    var dir_x = mouseX - x_pos;
+    var dir_y = mouseY - y_pos;
+
+    var mag = sqrt(dir_x * dir_x + dir_y * dir_y)
+
+    var vel_x = dir_x / mag * bullet_speed;
+    var vel_y = dir_y / mag * bullet_speed;
+
+    return new Bullet(x_pos, y_pos, vel_x, vel_y);
+}
 
 function mouseClicked()
 {
     if (mouseButton == LEFT)
     {
-	var dir_x = mouseX - x_pos;
-	var dir_y = mouseY - y_pos;
-
-	var mag = sqrt(dir_x * dir_x + dir_y * dir_y)
-
-	var vel_x = dir_x / mag * bullet_speed;
-	var vel_y = dir_y / mag * bullet_speed;
+	stompClient.send('/ws/gameBullets',{},JSON.stringify({x: x_pos,y:y_pos,velx: vel_x, vely:vel_y}));
 	
-	// bullets.push(new Bullet(x_pos, y_pos, vel_x, vel_y))
-    stompClient.send('/ws/gameBullets',{},JSON.stringify({x: x_pos,y:y_pos,velx: vel_x, vely:vel_y}));
+	let bullet = spawnBullet();
+	bullets.push(bullet)
     }
 }
